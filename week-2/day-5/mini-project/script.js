6f957895525ba889dacf2282983be602861a583a
nathaniel melloul <melloul.nathaniel@gmail.com>
2020-06-28 00:51:20 +0300

    calculator fist commit

--- week-2/day-5/mini-project/script.js
+++ week-2/day-5/mini-project/script.js
@@ -1,3 +1,77 @@
-hello world 
 
-my name is ........
+
+
+let nbr1 == nbr1() ;
+let nbr2 == nbr2() ; 
+let operator== {"+" , "-" , "*"  }
+let my_f()=== 'number'
+let my_op()==='string'
+
+
+
+function nbr1 (){
+
+	if  typeof my_f()=== 'number' { continue
+	} else { function operator()}
+}
+
+
+                                                      
+function operator () {
+
+	if typeof my_op()=== "string" && !" = " else {
+		function nbr2()
+	}
+}
+
+function nbr2 (){
+	 if typeof my_f()==='number'{
+		continue 
+	} if else { typeof my_op() ==="string" && ! " ="}
+	else { my_finish ()}
+}
+
+function equal () { 
+if button=== my_finish()
+}
+
+function global (){
+
+	nbr1() + operator() + nbr2()
+}
+
+
+
+ 
+ 
+// ass de nombre + operator + assoc 2em nombre 
+
+// }
+
+
+//
+
+
+
+
+
+
+// let texte = "ckdkjcdkjcdkajcdkjcdckja" ;
+
+// let newlist= typeof() ; 
+
+// console.log (newlist) ;
+
+
+// var index = 8;
+//   var result = (typeof index == 'string');
+//   console.log(result);
+//   // Output: true
+// var description = "w3resource";
+//   var result = (typeof description == 'string');
+//   alert(result); 
+
+let sese= "xyz" ;
+
+if (typeof "xyz" === "undefined")alert("Varaible is undefined");else alert("Variable is defined"); 
+

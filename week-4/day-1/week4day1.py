473da00675034a8581ae2239935d5ccb9a5dc86c
nathaniel-prog <66939923+nathaniel-prog@users.noreply.github.com>
2020-07-10 12:19:51 +0300

    rrrr

---
+++ week-4/day-1/week4day1.py
@@ -0,0 +1,55 @@
+#exercice1
+
+h= "helloworld\n "
+
+print(h*5)
+
+#exercice2
+
+sum=(99^3)*8
+print(sum)
+
+#exercice3
+
+info= ("Melloul","31","44")
+print( " your name is : ", info[0], "your age is :",info[1], "your size shoes is :", info[2])
+
+surname ="Nathaniel"
+age = 31
+shoe_size = 45
+
+inf = [surname , age , shoe_size]
+print(f"your surname is {surname},your age is {age},your size is {shoe_size}")
+
+# Exercice 4
+
+
+computer_brand = "ASUS VivoBook"
+print(f" i have a {computer_brand} computer " )
+
+# exercice 5
+#false
+#true
+#true
+#Error
+#true
+
+# exerice 6
+
+heigth_requested = 35
+user = input(("how tall are you?"))
+if int(user) >= heigth_requested:
+    print(" enjoy your ride ")
+else :
+    print("sorry but you are not tall enough to ride ")
+
+
+
+# Exercice 7
+
+user_number = input("choose an number")
+if int(user_number)%2==0:
+    print("this is an even number")
+else :
+    print("your number is not even")
+
